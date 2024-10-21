5e636bb7b941278ea786da2d24fec7d80094008b
Matthew Prem <matthew.r.prem@gmail.com>
2024-10-21 10:24:02 -0400

    file loading

--- main.py
+++ main.py
@@ -6,14 +6,30 @@ def zeroPadLeft(size, index):
         index = "0"+index
     return index
 
-startPath = "data\\20241008_07in_A310_NGC604\\science\\NGC 604-"
+lightPath = "data\\20241008_07in_A310_NGC604\\science\\NGC 604-"
 hLights = []
 oLights = []
 for i in range(1,11):
     s = zeroPadLeft(3, i)
-    hPath = startPath + s + "-ha.fit"
-    oPath = startPath + s + "-o.fit"
+    hPath = lightPath + s + "-ha.fit"
+    oPath = lightPath + s + "-o.fit"
     hLights.append(fits.open(hPath)[0])
     oLights.append(fits.open(oPath)[0])
     
+biasPath = "data\\20241008_07in_A310_NGC604\\calibration\\calib-"
+flatPath = "data\\20241008_07in_A310_NGC604\\calibration\\flat-"
+biases = []
+darks = []
+flatsH = []
+flatsO = []
+for i in range(1, 8):
+    s = zeroPadLeft(3,i)
+    biPath = biasPath + s + "-bi.fit"
+    dPath = biasPath + s + "-d.fit"
+    flatHPath = flatPath + s + "-ha.fit"
+    flatOPath = flatPath + s + "-o.fit"
+    biases.append(fits.open(biPath)[0])
+    darks.append(fits.open(dPath)[0])
+    flatsH.append(fits.open(flatHPath)[0])
+    flatsO.append(fits.open(flatOPath)[0])
 
