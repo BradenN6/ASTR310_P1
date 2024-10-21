363f116d66792f402cee59cd27b738116f53fa5c
Braden Nowicki <91173837+BradenN6@users.noreply.github.com>
2024-10-21 12:39:44 -0400

    Update calibration.py

--- calibration.py
+++ calibration.py
@@ -14,24 +14,25 @@ def display_img(file, vmin, vmax):
     fig = plt.imshow(img.data,vmin=vmin*np.min(img),vmax=vmax*np.max(img),cmap='plasma')
     plt.colorbar(fig,fraction=0.046,pad=0.04)
 
-# Median combine an array of images as FITS files
-def median_combine(files):
-    imgs = [file.data for file in files]
-
+# Median combine an array of images as FITS data arrays
+def median_combine(imgs):
     stack = np.dstack(tuple(imgs))
     master = np.median(stack,axis=2)
 
     return master
 
-def calib_bias(bias_files):
-    master_bias = median_combine(bias_files)
+# input open fits files in primary hdu [0]
+# filter as string 'ha' or 'oiii'
+def calib_bias(bias_files, filter):
+    bias_imgs = [bias_img.data for bias_img in bias_files]
+    master_bias = median_combine(bias_imgs)
     hdu = fits.PrimaryHDU(master_bias)
-    hdu.writeto('master_bias.fits', overwrite=True)
+    hdu.writeto('master_bias_' + filter + '.fit', overwrite=True)
 
 # Calibrate dark images
 # Subtract the master bias from each raw dark image
 # then median combine into a master dark. 
-def calib_darks(dark_files, master_bias_fname):
+def calib_darks(dark_files, master_bias_fname, filter):
     raw_darks = [file.data for file in dark_files]
 
     with fits.open(master_bias_fname) as mb:
@@ -41,10 +42,9 @@ def calib_darks(dark_files, master_bias_fname):
     master_dark = median_combine(bias_subtracted_darks)
 
     hdu = fits.PrimaryHDU(master_dark)
-    hdu.writeto('master_dark.fits', overwrite=True)
+    hdu.writeto('master_dark_' + filter + '.fit', overwrite=True)
 
-    md = fits.open("master_dark.fit")[0]
-    with fits.open("master_dark.fits") as hdul:
+    with fits.open('master_dark_' + filter + '.fit') as hdul:
         hdr = hdul[0].header
         hdr["EXPTIME"] = dark_files.header["EXPTIME"]
 
@@ -52,16 +52,27 @@ def calib_darks(dark_files, master_bias_fname):
 # Subtract master bias from each flat image
 # Subtract master dark scaled by exposure time
 # Median combine and normalize by dividing by median
-def calib_flats(flat_files, master_bias_fname, master_dark_fname):
+def calib_flats(flat_files, master_bias_fname, master_dark_fname, filter):
     #bias_subtracted_flats = [flat - master_bias for flat in raw_flats]
     #dark_subtracted_flats = [flat - master_dark*flat.header["EXPTIME"]/dark.header["EXPTIME"]]
     with fits.open(master_bias_fname) as mb:
         master_bias = mb[0].data
 
     with fits.open(master_dark_fname) as md:
-        master_bias = md[0].data
+        master_dark = md[0].data
+        dark_exptime = md[0].header['EXPTIME']
+
+    calibrated_flats = []
 
     for raw_flat in flat_files:
         bias_subtracted_flat = raw_flat.data - master_bias
-        dark_subtracted_flat = bias_subtracted_flat - master_dark*raw_flat.header["EXPTIME"]/master_dark.header["EXPTIME"]
-        # get master dark with FITS header
+        dark_subtracted_flat = bias_subtracted_flat - master_dark*raw_flat.header["EXPTIME"]/dark_exptime
+        calibrated_flats.append(dark_subtracted_flat)
+
+    median_combined_flats = median_combine(calibrated_flats)
+
+    # Normalized master flat
+    master_flat = median_combined_flats/np.median(median_combined_flats)
+
+    hdu = fits.PrimaryHDU(master_flat)
+    hdu.writeto('master_flat_' + filter + '.fits', overwrite=True)
