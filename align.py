d8f966cc7c006ef1a51d68a17ca3a6a4ee24bfc0
Matthew Prem <matthew.r.prem@gmail.com>
2024-10-28 10:48:44 -0400

    Align code finished

--- align.py
+++ align.py
@@ -1,5 +1,9 @@
 # from imshift.py
 import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.patches as patches
+import main
+from astropy.io import fits
 
 def imshift(im,nr,nc):
     """Shifts an image by nr rows and nc columns
@@ -29,7 +33,22 @@ def findMaxPixelCoord(data, guessX, guessY, rX, rY):
                 maxCoord = (x,y)
     return maxCoord[0], maxCoord[1]
 
+#function which takes in an array of calibrated light frames and returns an array of aligned frames
 def alignFrames(hduArray, guessX, guessY, rX=10, rY=10, markup = False):
     shifts = []
     x0, y0 = findMaxPixelCoord(hduArray[0].data, guessX, guessY, rX, rY)
-    for i
+    testX, testY = x0, y0
+    for i in range(1, len(hduArray)):
+        x, y = findMaxPixelCoord(hduArray[i].data, testX, testY, rX, rY)
+        shifts.append([testX-x,testY-y])
+        if markup:
+            main.dispFITS(hduArray[i],1, 10)
+            plt.gca().add_patch(patches.Rectangle((testX-rX, testY-rY),2*rX, 2*rY), color="red", fill=False)
+            plt.gca().add_patch(patches.Rectangle((x, y), 1, 1), color="black", fc=(0,0,0,0.3))
+        testX += testX-x
+        testY += testY-y
+    shifted = []
+    for img in hduArray:
+        shifted.append(fits.ImageHDU(imshift(img.data, shifts[i][0], shifts[i][1]),img.header))
+    return shifted
+            
--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from astropy.io import fits
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.patches as patches
+import align
 
 import calibration as calib
 
