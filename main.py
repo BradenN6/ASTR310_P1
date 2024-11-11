b10af42a648bfcf166b92c4f0b97314d8eabb8d7
Matthew Prem <matthew.r.prem@gmail.com>
2024-11-11 10:06:06 -0500

    angle code

--- main.py
+++ main.py
@@ -69,11 +69,42 @@ SCO3 = (1225, 890)
 # dispFITS(addedFits, 1, 1, "Added Session 3 O[III]")
 # fits.writeto("DS-3_OIII.fit",addedFits.data, addedFits.header, overwrite=True)
 
+
+Ha1 = fits.open("DS-1_Ha.fit")[0]
+Ha2 = fits.open("DS-2_Ha.fit")[0]
+Ha3 = fits.open("DS-3_Ha.fit")[0]
+O1 = fits.open("DS-1_OIII.fit")[0]
+O2 = fits.open("DS-2_OIII.fit")[0]
+O3 = fits.open("DS-3_OIII.fit")[0]
+
+
+# dispFITS(Ha1, 1, 10)
+# dispFITS(Ha2, 1, 10)
+# dispFITS(Ha3, 1, 10)
+
+RS1 = (660, 547)
+RS2 = (401, 622)
+RS3 = (388, 602)
+
+H1x2, H1y2 = align.findMaxPixelCoord(Ha1.data, RS1[0], RS1[1],10,10)
+H2x2, H2y2 = align.findMaxPixelCoord(Ha2.data, RS2[0], RS2[1],10,10)
+H3x2, H3y2 = align.findMaxPixelCoord(Ha3.data, RS3[0], RS3[1],10,10)
+
+H1x, H1y = align.findMaxPixelCoord(Ha1.data, SCH1[0],SCH1[1],10,10)
+H2x, H2y = align.findMaxPixelCoord(Ha2.data, SCH2[0],SCH2[1],10,10)
+H3x, H3y = align.findMaxPixelCoord(Ha3.data, SCH3[0],SCH3[1],10,10)
+
+shift2 = (H1x-H2x,H1y-H2y)
+shift3 = (H1x-H3x,H1y-H3y)
+angle1 = np.atan2(H1y2-H1y, H1x2-H1x)
+angle2 = np.atan2(H2y2-H2y, H2x2-H2x)
+angle3 = np.atan2(H3y2-H3y, H3x2-H3x)
+rot2 = angle2-angle1
+rot3 = angle3-angle1
+
+shifted = [Ha1]
+shifted.append(fits.ImageHDU(align.imshift(Ha2.data, shift2[1], shift2[0]),Ha2.header))
+shifted.append(fits.ImageHDU(align.imshift(Ha3.data, shift3[1], shift3[0]),Ha3.header))
+H = dataReduce.sum(shifted)
+dispFITS(H,1,1)
 plt.show()
-Ha1 = fits.open("DS-1_Ha.fit")
-Ha2 = fits.open("DS-2_Ha.fit")
-Ha3 = fits.open("DS-3_Ha.fit")
-O1 = fits.open("DS-1_OIII.fit")
-O2 = fits.open("DS-2_OIII.fit")
-O3 = fits.open("DS-3_OIII.fit")
-dispFITS()
