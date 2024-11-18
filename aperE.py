64830417b24dba661f1143e582d15de3613b305c
Braden Nowicki <91173837+BradenN6@users.noreply.github.com>
2024-11-18 09:32:23 -0500

    brightness

--- aperE.py
+++ aperE.py
@@ -4,7 +4,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.patches as patches
 
-def aperE(im, col, row, rad1, rad2, ir1, ir2, or1, or2, Kccd, saturation=np.inf):
+def aperE(im, col, row, rad1, rad2, ir1, ir2, or1, or2, Kccd, markup=True, saturation=np.inf):
     """Original code by Professor Alberto Bolatto, edited by Alyssa Pagan, and
     translated to Python by ChongChong He, further edited by Orion Guiffreda.
 
@@ -54,29 +54,31 @@ def aperE(im, col, row, rad1, rad2, ir1, ir2, or1, or2, Kccd, saturation=np.inf)
     px = np.reshape(xx[ix], (bb, aa))
     py = np.reshape(yy[ix], (bb, aa))
     pz = np.reshape(im[ix], (bb, aa))
-    plt.figure()
-    plt.imshow(pz, extent=[px[0, 0], px[0, -1], py[0, 0], py[-1, 0]])
-    plt.tight_layout()
-    # if not np.isempty(imixsrc):
-    #     np.caxis(np.concatenate((sky, np.array([max(imixsrc)]))))
 
-    p = np.arange(360) * np.pi / 180
-    xc = np.cos(p)
-    yc = np.sin(p)
-    plt.plot(col+rad1*xc, row+rad2*yc, 'w')
-    plt.plot(col+ir1*xc, row+ir2*yc, 'r')
-    plt.plot(col+or1*xc, row+or2*yc, 'r')
-    if issat:
-        plt.text(col, row, 'CHECK SATURATION', ha='center', color='w',
-                 va='top', fontweight='bold')
-        print('At the peak this source has {:0.0f} counts.'.format(
-            max(imixsrc)))
-        print('Judging by the number of counts, if this is a single exposure the')
-        print('source is likely to be saturated. If this is the coadding of many')
-        print('short exposures, check in one of them to see if this message appears.')
-        print('If it does, you need to flag the source as bad in this output file.')
-    plt.tight_layout()
-    #plt.savefig("aperE_img.pdf")
+    if markup:
+        plt.figure()
+        plt.imshow(pz, extent=[px[0, 0], px[0, -1], py[0, 0], py[-1, 0]])
+        plt.tight_layout()
+        # if not np.isempty(imixsrc):
+        #     np.caxis(np.concatenate((sky, np.array([max(imixsrc)]))))
+
+        p = np.arange(360) * np.pi / 180
+        xc = np.cos(p)
+        yc = np.sin(p)
+        plt.plot(col+rad1*xc, row+rad2*yc, 'w')
+        plt.plot(col+ir1*xc, row+ir2*yc, 'r')
+        plt.plot(col+or1*xc, row+or2*yc, 'r')
+        if issat:
+            plt.text(col, row, 'CHECK SATURATION', ha='center', color='w',
+                     va='top', fontweight='bold')
+            print('At the peak this source has {:0.0f} counts.'.format(
+                max(imixsrc)))
+            print('Judging by the number of counts, if this is a single exposure the')
+            print('source is likely to be saturated. If this is the coadding of many')
+            print('short exposures, check in one of them to see if this message appears.')
+            print('If it does, you need to flag the source as bad in this output file.')
+        plt.tight_layout()
+        #plt.savefig("aperE_img.pdf")
     return flx, err
 
 
